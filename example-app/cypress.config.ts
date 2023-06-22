a802ca17c68cbf9c69e18fb94d869c03dd836c6e
Anton Gustafsson <anton@moimob.com>
2023-06-22 16:31:59 +0200

    fix

--- example-app/cypress.config.ts
+++ example-app/cypress.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'cypress';
-import webpackConfig from './webpack.config.js';
 
 export default defineConfig({
     video: false,
@@ -15,8 +14,7 @@ export default defineConfig({
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             framework: 'cypress-ct-aurelia',
-            bundler: 'webpack',
-            webpackConfig
+            bundler: 'webpack'
         }
     }
 });
