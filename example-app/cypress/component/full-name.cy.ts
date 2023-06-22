dbc382ff554d4692f4d7ddb065eb46414be68cf0
Anton Gustafsson <anton@moimob.com>
2023-06-22 14:06:42 +0200

    chore: more cleaning

--- example-app/cypress/component/full-name.cy.ts
+++ example-app/cypress/component/full-name.cy.ts
@@ -1,16 +1,20 @@
 import { StageComponent } from 'aurelia-testing';
+import { Container } from 'aurelia-dependency-injection';
+import { FullName } from '../../src/components/full-name';
 
 describe('full-name', () => {
     it('should load', () => {
-        const component = StageComponent.withResources('resources/full-name')
+        const component = StageComponent.withResources('components/full-name')
             .inView('<full-name first-name.bind="firstName" last-name.bind="lastName"></full-name>')
             .boundTo({ firstName: 'Bob', lastName: 'Smith' });
+
+        const container = new Container();
+        container.get(FullName);
 
         cy.mount(component);
 
         cy.get(`[data-cy=first-name]`).should('have.text', 'Bob');
         cy.get(`[data-cy=last-name]`).should('have.text', 'Smith');
-
         cy.get(`[data-cy=full-name]`).should('have.text', 'Bob Smith');
     });
 });
--- example-app/src/resources/index.ts
+++
@@ -1,6 +0,0 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
-import { PLATFORM } from 'aurelia-pal';
-
-export function configure(config: FrameworkConfiguration): void {
-    config.globalResources([PLATFORM.moduleName('./full-name')]);
-}
